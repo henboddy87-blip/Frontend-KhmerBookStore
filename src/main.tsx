8de731f55b784a69238c8022dc2d7d1306a6f133
unknown <henboddy87@gmail.com>
2026-08-24 20:44:55 +0700

    feat: support VITE_GOOGLE_CLIENT_ID env variable for Google OAuth

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import { App } from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "769811339391-6ik8gje92rkkp4qvefgiuknolkeqbvg7.apps.googleusercontent.com";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="769811339391-6ik8gje92rkkp4qvefgiuknolkeqbvg7.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={googleClientId}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>
